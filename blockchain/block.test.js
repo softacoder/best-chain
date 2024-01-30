bb96e7ab09c254cb54cbb4a9dd2f541c81079d44
softacoder <113185323+softacoder@users.noreply.github.com>
2024-01-30 14:21:07 +0000

    commit test difficulty adjustment

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,6 +1,8 @@
 const Block = require("./block");
-let data, lastBlock, block;
+
 describe("Block", () => {
+  let data, lastBlock, block;
+
   beforeEach(() => {
     data = "bar";
     lastBlock = Block.genesis();
@@ -14,4 +16,23 @@ describe("Block", () => {
   it("sets the `lastHash` to match the hash of the last block", () => {
     expect(block.lastHash).toEqual(lastBlock.hash);
   });
+
+  it("generates a hash that matches the difficulty", () => {
+    expect(block.hash.substring(0, block.difficulty)).toEqual(
+      "0".repeat(block.difficulty)
+    );
+  });
+
+  it("lowers the difficulty for slowly mined blocks", () => {
+    expect(Block.adjustDifficulty(block, block.timestamp + 360000)).toEqual(
+      block.difficulty - 1
+    );
+  });
+  it("raises the difficulty for quickly mined blocks", () => {
+    expect(Block.adjustDifficulty(block, block.timestamp + 1)).toEqual(
+      block.difficulty + 1
+    );
+  });
 });
+
+// module.exports = Block.test;
