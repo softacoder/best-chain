a6e16985e2db858b3263f1b2452ca75bfabda895
softacoder <113185323+softacoder@users.noreply.github.com>
2024-02-06 15:38:43 +0000

    commit create a transaction

--- chain-util.js
+++ chain-util.js
@@ -1,9 +1,14 @@
 const EC = require("elliptic").ec;
+const uuidV1 = require("uuid/v1");
 const ec = new EC("sep256k1");
 
 class ChainUtil {
   static genKeyPair() {
     return ec.genKeyPair();
+  }
+
+  static id() {
+    return uuidV1();
   }
 }
 
