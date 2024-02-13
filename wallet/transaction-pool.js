7150e7078f1ce9ac86fefd327d3b52f9d949cc8c
softacoder <113185323+softacoder@users.noreply.github.com>
2024-02-13 09:41:41 +0000

    commit grad valid transactions

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,5 @@
+const Transaction = require("../wallet/");
+
 class TransactionPool {
   constructor() {
     this.transaction = [];
@@ -14,10 +16,31 @@ class TransactionPool {
       this.transactions.push(transaction);
     }
   }
+
   existingTransaction(address) {
     return this.transactions.find(
       (transaction) => transaction.input.address === address
     );
+  }
+
+  validTransactions() {
+    return this.transactions.filter((transaction) => {
+      const outputTotal = transaction.outputs.reduce((total, output) => {
+        return total + output.amount;
+      }, 0);
+
+      if (transaction.input.amount !== outputTotal) {
+        console.log(`Invalid transaction from ${transaction.input.address}.`);
+        return;
+      }
+
+      if (!Transaction.verifyTransaction(transaction)) {
+        console.log(`Invalid signature from ${transaction.input.address}.`);
+        return;
+      }
+
+      return transaction;
+    });
   }
 }
 
