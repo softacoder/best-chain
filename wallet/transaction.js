ab202b5b758a2d8754d47ae57d796d5c32d4aca6
softacoder <113185323+softacoder@users.noreply.github.com>
2024-02-14 15:28:55 +0000

    commit test reward transaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const ChainUtil = require("..chain-util");
+const { MINING_REWARD } = require("../config");
 
 class Transaction {
   constructor() {
@@ -24,27 +25,37 @@ class Transaction {
     return this;
   }
 
-  static newTransaction(senderWallet, recipient, amount) {
+  static transactionWithoutOutputs(senderWallet, recipient, amount) {
     const transaction = new this();
+    transaction.outputs.push(...outputs);
+    Transaction.signTransaction(transaction, senderWallet);
+    return transaction;
+  }
 
+  static newTransaction(senderWallet, recipient, amount) {
     if (amount > senderWallet.balance) {
       console.log("Amount: ${amount} exceeds balance.");
       return;
     }
 
-    transaction.outputs.push(
-      ...[
-        {
-          amount: senderWallet.balance - amount,
-          address: senderWallet.publicKey,
-        },
-        { amount, address: recipient },
-      ]
-    );
-    Transaction.signTransaction(transaction, senderWallet);
-
-    return transaction;
+    return this.transactionWithoutOutputs(senderWallet, [
+      {
+        amount: senderWallet.balance - amount,
+        address: senderWallet.publicKey,
+      },
+      { amount, address: recipient },
+    ]);
   }
+
+  static rewardTransaction(minerWallet, blockchainWallet) {
+    return Transaction.transactionWithoutOutputs(blockchainWallet, [
+      {
+        amount: MINING_REWARD,
+        address: minerWallet.publicKey,
+      },
+    ]);
+  }
+
   static signTransaction(transaction, senderWallet) {
     transaction.input = {
       timestamp: Date.now(),
