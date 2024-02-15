4cb367e983f45cbd08db9150aba07f3dba96c28f
softacoder <113185323+softacoder@users.noreply.github.com>
2024-02-15 20:36:50 +0000

    commit last

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,13 +1,15 @@
 const TransactionPool = require("./transaction-pool");
 const Transaction = require("./transaction");
 const Wallet = require("./index");
+const Blockchain = require("../blockchain");
 
 describe("TransactionPool", () => {
-  let tp, wallet, transaction;
+  let tp, wallet, transaction, bc;
 
   beforeEach(() => {
     tp = new TransactionPool();
     wallet = new Wallet();
+    bc = new Blockchain();
     transaction = wallet.createTransaction("r4nd-4dr355", 30, tp);
   });
 
@@ -38,7 +40,7 @@ describe("TransactionPool", () => {
       validTransactions = [...tp.transactions];
       for (let i = 0; i < 6; i++) {
         wallet = new Wallet();
-        transaction = wallet.createTransaction("r4ddr355", 30, tp);
+        transaction = wallet.createTransaction("r4ddr355", 30, bc, tp);
         if (i % 2 == 0) {
           transaction.input.amount = 99999;
         } else {
